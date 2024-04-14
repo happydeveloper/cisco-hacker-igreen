34861508d557c3801c2771910b7e0ba29e5abf21
duru <enfn2001@gmail.com>
2024-04-14 20:45:20 +0900

    cisco 해커톤 데모 - iGreen 봇

--- README.md
+++ README.md
@@ -1,44 +1,17 @@
-# Webex-Bot-Starter
+# Webex Cisco Hacker톤 
 
-### Starter kit and template for a simple Webex bot
+### 식물에 음약을 듣기
 
-_For a more detailed walkthorugh, see the [companion blog post here](https://developer.webex.com/blog/from-zero-to-webex-teams-chatbot-in-15-minutes)_
+식물을 전파값을 측정하고 
+해당 측정값을 AI 를 통해 음악을 생성 (가정)
+음악을 서버에 유튜브에 올림 (가정)
 
-This is a very simple Webex node.JS bot application that serves as a template to be further extended. It features the [webex-node-bot-framework](https://github.com/webex/webex-bot-node-framework) that simplifies development for Webex bots by abstracting away some of the complexity of the API calls and registering for events. Some parts of the app are taken from on the old [sparkbotstarter](https://github.com/valgaze/sparkbotstarter) template created by Victor Algaze.
+미리 정의된 @iGreen 웹벡스 봇이 plant 명령을 내리면 식물의 음악을 들을 수 있게 음악링크를 제공
+타이머 주기별로 재생 (todo : 구현 예정)
 
-Here is the bot in action:
 
-git
-![What we're making](./images/webexbotstarter.gif)
+```
+npm run start
 
-## Prerequisites:
-
-- [ ] node.js (minimum supported v8.0.0 & npm 2.14.12 and up)
-
-- [ ] [Sign up for Webex Developer Account](https://developer.webex.com/signup)
-
----
-
-## Steps to get the bot working
-
-1. Create a Webex bot (save the API access token and username): https://developer.webex.com/my-apps/new/bot
-
-2. Sign up for nGrok, then connect and start it on your machine (save the port number and public web address): https://ngrok.com/download
-
-3. After installing ngrok, run it on your local machine to get a public ip address, eg `ngrok http 3000 --region=eu`
-
-4. Copy the ip address displayed in the ngrok window, ie: : https://1234.eu.ngrok.io
-
-5. Copy the `.env.local` file to a file called `.env`
-
-6. Edit `.env` with the following values:
-
-- BOTTOKEN - Set this to the token for your bot that you got in step 1
-- PORT - Set this to the port you set when you started ngrok in step 3 (ie: 3000)
-- WEBHOOKURL - Set this to the ip address that you copied in step 4
-
-5. Turn on your bot server with `npm start`
-
-6. Create a space in Webex
-
-7. Add the bot (by its username) to the space in Webex
+@iGreen plnat ##  식물을 음악을 들려줘
+```
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//Webex Bot Starter - featuring the webex-node-bot-framework - https://www.npmjs.com/package/webex-node-bot-framework
 require("dotenv").config();
 var framework = require("webex-node-bot-framework");
 var webhook = require("webex-node-bot-framework/webhook");
@@ -13,161 +12,116 @@ const config = {
   port: process.env.PORT,
 };
 
-// init framework
+// 프레임워크 초기화
 var framework = new framework(config);
 framework.start();
-console.log("Starting framework, please wait...");
+console.log("프레임워크 시작 중, 잠시만 기다려주세요...");
 
 framework.on("initialized", () => {
-  console.log("framework is all fired up! [Press CTRL-C to quit]");
+  console.log("프레임워크 준비 완료! [종료하려면 CTRL-C를 누르세요]");
 });
 
-// A spawn event is generated when the framework finds a space with your bot in it
-// If actorId is set, it means that user has just added your bot to a new space
-// If not, the framework has discovered your bot in an existing space
 framework.on("spawn", (bot, id, actorId) => {
   if (!actorId) {
-    // don't say anything here or your bot's spaces will get
-    // spammed every time your server is restarted
-    console.log(
-      `While starting up, the framework found our bot in a space called: ${bot.room.title}`
-    );
+    console.log(`서버 재시작시 발견된 공간: ${bot.room.title}`);
   } else {
-    // When actorId is present it means someone added your bot got added to a new space
-    // Lets find out more about them..
-    var msg =
-      "You can say `help` to get the list of words I am able to respond to.";
+    var msg = "도움말을 보려면 'help'라고 말해주세요.";
     bot.webex.people
       .get(actorId)
       .then((user) => {
-        msg = `Hello there ${user.displayName}. ${msg}`;
+        msg = `안녕하세요 ${user.displayName}. ${msg}`;
       })
       .catch((e) => {
-        console.error(
-          `Failed to lookup user details in framwork.on("spawn"): ${e.message}`
-        );
-        msg = `Hello there. ${msg}`;
+        console.error(`사용자 정보 조회 실패: ${e.message}`);
+        msg = `안녕하세요. ${msg}`;
       })
       .finally(() => {
-        // Say hello, and tell users what you do!
         if (bot.isDirect) {
           bot.say("markdown", msg);
         } else {
           let botName = bot.person.displayName;
-          msg += `\n\nDon't forget, in order for me to see your messages in this group space, be sure to *@mention* ${botName}.`;
+          msg += `\n\n이 그룹 공간에서 메시지를 보려면 *@${botName}*을 언급해주세요.`;
           bot.say("markdown", msg);
         }
       });
   }
 });
 
-// Implementing a framework.on('log') handler allows you to capture
-// events emitted from the framework.  Its a handy way to better understand
-// what the framework is doing when first getting started, and a great
-// way to troubleshoot issues.
-// You may wish to disable this for production apps
 framework.on("log", (msg) => {
   console.log(msg);
 });
 
-// Process incoming messages
-// Each hears() call includes the phrase to match, and the function to call if webex mesages
-// to the bot match that phrase.
-// An optional 3rd parameter can be a help string used by the frameworks.showHelp message.
-// An optional fourth (or 3rd param if no help message is supplied) is an integer that
-// specifies priority.   If multiple handlers match they will all be called unless the priority
-// was specified, in which case, only the handler(s) with the lowest priority will be called
-
-/* On mention with command
-ex User enters @botname framework, the bot will write back in markdown
-*/
 framework.hears(
   "framework",
   (bot) => {
-    console.log("framework command received");
+    console.log("framework 명령어 받음");
     bot.say(
       "markdown",
-      "The primary purpose for the [webex-node-bot-framework](https://github.com/jpjpjp/webex-node-bot-framework) was to create a framework based on the [webex-jssdk](https://webex.github.io/webex-js-sdk) which continues to be supported as new features and functionality are added to Webex. This version of the project was designed with two themes in mind: \n\n\n * Mimimize Webex API Calls. The original flint could be quite slow as it attempted to provide bot developers rich details about the space, membership, message and message author. This version eliminates some of that data in the interests of efficiency, (but provides convenience methods to enable bot developers to get this information if it is required)\n * Leverage native Webex data types. The original flint would copy details from the webex objects such as message and person into various flint objects. This version simply attaches the native Webex objects. This increases the framework's efficiency and makes it future proof as new attributes are added to the various webex DTOs "
+      "이 프레임워크의 주요 목적은 계속해서 새로운 기능이 추가되는 Webex에 대응하여 [webex-js-sdk](https://webex.github.io/webex-js-sdk) 기반의 프레임워크를 만드는 것입니다. 불필요한 Webex API 호출을 최소화하고, Webex 데이터 유형을 활용하여 효율성을 높이고 미래 기능 확장을 용이하게 하는 두 가지 테마를 중심으로 설계되었습니다."
     );
   },
-  "**framework**: (learn more about the Webex Bot Framework)",
+  "**framework**: (Webex Bot Framework에 대해 알아보기)",
   0
 );
 
-/* On mention with command, using other trigger data, can use lite markdown formatting
-ex User enters @botname 'info' phrase, the bot will provide personal details
-*/
 framework.hears(
   "info",
   (bot, trigger) => {
-    console.log("info command received");
-    //the "trigger" parameter gives you access to data about the user who entered the command
+    console.log("info 명령어 받음");
     let personAvatar = trigger.person.avatar;
     let personEmail = trigger.person.emails[0];
     let personDisplayName = trigger.person.displayName;
-    let outputString = `Here is your personal information: \n\n\n **Name:** ${personDisplayName}  \n\n\n **Email:** ${personEmail} \n\n\n **Avatar URL:** ${personAvatar}`;
+    let outputString = `여기에 당신의 개인 정보가 있습니다: \n\n\n **이름:** ${personDisplayName}  \n\n\n **이메일:** ${personEmail} \n\n\n **아바타 URL:** ${personAvatar}`;
     bot.say("markdown", outputString);
   },
-  "**info**: (get your personal details)",
+  "**info**: (개인 정보 확인)",
   0
 );
 
-/* On mention with bot data 
-ex User enters @botname 'space' phrase, the bot will provide details about that particular space
-*/
 framework.hears(
   "space",
   (bot) => {
-    console.log("space. the final frontier");
+    console.log("space, 끝없는 공간");
     let roomTitle = bot.room.title;
     let spaceID = bot.room.id;
     let roomType = bot.room.type;
 
-    let outputString = `The title of this space: ${roomTitle} \n\n The roomID of this space: ${spaceID} \n\n The type of this space: ${roomType}`;
+    let outputString = `이 공간의 제목: ${roomTitle} \n\n 공간 ID: ${spaceID} \n\n 공간 유형: ${roomType}`;
 
     console.log(outputString);
     bot
       .say("markdown", outputString)
-      .catch((e) => console.error(`bot.say failed: ${e.message}`));
+      .catch((e) => console.error(`bot.say 실패: ${e.message}`));
   },
-  "**space**: (get details about this space) ",
+  "**space**: (이 공간에 대한 정보 얻기)",
   0
 );
 
-/* 
-   Say hi to every member in the space
-   This demonstrates how developers can access the webex
-   sdk to call any Webex API.  API Doc: https://webex.github.io/webex-js-sdk/api/
-*/
 framework.hears(
   "say hi to everyone",
   (bot) => {
-    console.log("say hi to everyone.  Its a party");
-    // Use the webex SDK to get the list of users in this space
+    console.log("모두에게 인사하기. 파티 시작!");
     bot.webex.memberships
       .list({ roomId: bot.room.id })
       .then((memberships) => {
         for (const member of memberships.items) {
           if (member.personId === bot.person.id) {
-            // Skip myself!
             continue;
           }
-          let displayName = member.personDisplayName
-            ? member.personDisplayName
-            : member.personEmail;
-          bot.say(`Hello ${displayName}`);
+          let displayName = member.personDisplayName ? member.personDisplayName : member.personEmail;
+          bot.say(`안녕 ${displayName}`);
         }
       })
       .catch((e) => {
-        console.error(`Call to sdk.memberships.get() failed: ${e.messages}`);
-        bot.say("Hello everybody!");
+        console.error(`sdk.memberships.list() 호출 실패: ${e.message}`);
+        bot.say("여러분 안녕하세요!");
       });
   },
-  "**say hi to everyone**: (everyone gets a greeting using a call to the Webex SDK)",
+  "**say hi to everyone**: (Webex SDK를 사용해 모두에게 인사하기)",
   0
 );
 
-// Buttons & Cards data
+// 버튼 및 카드 데이터
 let cardJSON = {
   $schema: "http://adaptivecards.io/schemas/adaptive-card.json",
   type: "AdaptiveCard",
@@ -209,13 +163,10 @@ let cardJSON = {
   ],
 };
 
-/* On mention with card example
-ex User enters @botname 'card me' phrase, the bot will produce a personalized card - https://developer.webex.com/docs/api/guides/cards
-*/
 framework.hears(
   "card me",
   (bot, trigger) => {
-    console.log("someone asked for a card");
+    console.log("누군가 카드를 요청했습니다");
     let avatar = trigger.person.avatar;
 
     cardJSON.body[0].columns[0].items[0].url = avatar
@@ -225,76 +176,31 @@ framework.hears(
     cardJSON.body[0].columns[0].items[2].text = trigger.person.emails[0];
     bot.sendCard(
       cardJSON,
-      "This is customizable fallback text for clients that do not support buttons & cards"
+      "이 카드는 버튼과 카드를 지원하지 않는 클라이언트를 위한 맞춤형 대체 텍스트입니다"
     );
   },
-  "**card me**: (a cool card!)",
+  "**card me**: (개인화된 카드 제공)",
   0
 );
 
-/* On mention reply example
-ex User enters @botname 'reply' phrase, the bot will post a threaded reply
-*/
 framework.hears(
-  "reply",
+  "plant",
   (bot, trigger) => {
-    console.log("someone asked for a reply.  We will give them two.");
+    console.log("노래 서버에서 제공");
     bot.reply(
       trigger.message,
-      "This is threaded reply sent using the `bot.reply()` method.",
+      "식물의 소리를 들려줘",
       "markdown"
     );
     var msg_attach = {
-      text: "This is also threaded reply with an attachment sent via bot.reply(): ",
-      file: "https://media2.giphy.com/media/dTJd5ygpxkzWo/giphy-downsized-medium.gif",
+      text: "식물에 음악을 들어보세요. https://www.youtube.com/watch?v=3EZqEvMoS3w",
+      file: "https://media1.giphy.com/avatars/botanicabar_/pyUP8mU6fxg4.jpg",
     };
     bot.reply(trigger.message, msg_attach);
   },
-  "**reply**: (have bot reply to your message)",
+  "**reply**: (메시지에 답장하기)",
   0
 );
-
-/* On mention with command
-ex User enters @botname help, the bot will write back in markdown
- *
- * The framework.showHelp method will use the help phrases supplied with the previous
- * framework.hears() commands
-*/
-framework.hears(
-  /help|what can i (do|say)|what (can|do) you do/i,
-  (bot, trigger) => {
-    console.log(`someone needs help! They asked ${trigger.text}`);
-    bot
-      .say(`Hello ${trigger.person.displayName}.`)
-      //    .then(() => sendHelp(bot))
-      .then(() => bot.say("markdown", framework.showHelp()))
-      .catch((e) => console.error(`Problem in help hander: ${e.message}`));
-  },
-  "**help**: (what you are reading now)",
-  0
-);
-
-/* On mention with unexpected bot command
-   Its a good practice is to gracefully handle unexpected input
-   Setting the priority to a higher number here ensures that other 
-   handlers with lower priority will be called instead if there is another match
-*/
-framework.hears(
-  /.*/,
-  (bot, trigger) => {
-    // This will fire for any input so only respond if we haven't already
-    console.log(`catch-all handler fired for user input: ${trigger.text}`);
-    bot
-      .say(`Sorry, I don't know how to respond to "${trigger.text}"`)
-      .then(() => bot.say("markdown", framework.showHelp()))
-      //    .then(() => sendHelp(bot))
-      .catch((e) =>
-        console.error(`Problem in the unexepected command hander: ${e.message}`)
-      );
-  },
-  99999
-);
-
 //Server config & housekeeping
 // Health Check
 app.get("/", (req, res) => {
